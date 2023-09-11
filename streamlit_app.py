fb59a6242e85cc13eaccfc437f6c45bbe08673d8
Band1kut <glukhead@gmail.com>
2023-09-11 14:53:54 +0300

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,13 @@ with st.echo():
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
     from selenium.webdriver.chrome.service import Service
+    # from webdriver_manager.chrome import ChromeDriverManager
     from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.core.os_manager import ChromeType
 
     def get_driver():
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        return webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
+        # return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     options = Options()
     options.add_argument('--disable-gpu')
